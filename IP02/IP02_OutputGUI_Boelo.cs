2b24e8917c29af974268a1bd35169fbd785e9690
Nicholas Kirk <nickmail13@yahoo.com>
2015-09-10 18:14:37 -0500

    Copied & renamed output GUI files for reuse & modification

---
+++ IP02/IP02_OutputGUI_Boelo.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IP02 {
+    public partial class OutputGUI : Form {
+        public OutputGUI() {
+            InitializeComponent();
+        }
+
+        /**
+        *   This displays the check in a window.
+        */
+        public void displayCheck(int checkID, String payee, double dollarAmount, String dollarText, String memo, int totalChecks, double dollarSum) {
+
+            // Get the date for the check
+            String day = DateTime.Now.Day.ToString();
+            String month = DateTime.Now.Month.ToString();
+            String date = (month.Length == 1 ? "0" + month : month) + "/"
+                + (day.Length == 1 ? "0" + day : day) + "/"
+                + DateTime.Now.Year;
+
+            checkNumTopLabel.Text = checkID.ToString();
+            checkNumBottomLabel.Text = checkID.ToString();
+            dateLabel.Text = date;
+
+            // Display the dollar amount correctly
+            dollarsLabel.Text = String.Format("{0:n}", dollarAmount);
+
+            // These loops make sure the underlined string reaches the desired length.
+            int payeeLength = payeeLabel.Text.Length;
+            payeeLabel.Text = payee;
+            while (payeeLabel.Text.Length < payeeLength)
+                payeeLabel.Text += " ";
+
+            int dollarTextLength = dollarsTextLabel.Text.Length;
+            dollarsTextLabel.Text = dollarText;
+            while (dollarsTextLabel.Text.Length < dollarTextLength)
+                dollarsTextLabel.Text += " ";
+
+            // This adjusts the window size to fit the dollar text
+            if (dollarsTextLabel.Text.Length > dollarTextLength)
+                this.Width += (dollarsTextLabel.Text.Length - dollarTextLength) * 9;
+
+            int memoLength = memoLabel.Text.Length;
+            memoLabel.Text = memo;
+            while (memoLabel.Text.Length < memoLength)
+                memoLabel.Text += " ";
+
+            totalsLabel.Text = "Total checks: " + totalChecks.ToString() + " | Sum of all checks: " + dollarSum.ToString();
+            // Show the window
+            Application.Run(this);
+        }
+
+        private void OutputGUI_Load(object sender, EventArgs e) {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e) {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e) {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e) {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e) {
+
+        }
+
+        private void label17_Click(object sender, EventArgs e) {
+
+        }
+    }
+}
--- IP02/OutputGUI.Designer.cs
+++ IP02/OutputGUI.Designer.cs
@@ -23,12 +23,279 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
-            this.components = new System.ComponentModel.Container();
+            this.checkNumTopLabel = new System.Windows.Forms.Label();
+            this.dateLabel = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dollarsLabel = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.payeeLabel = new System.Windows.Forms.Label();
+            this.dollarsTextLabel = new System.Windows.Forms.Label();
+            this.memoLabel = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.label16 = new System.Windows.Forms.Label();
+            this.checkNumBottomLabel = new System.Windows.Forms.Label();
+            this.label20 = new System.Windows.Forms.Label();
+            this.totalsLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // checkNumTopLabel
+            // 
+            this.checkNumTopLabel.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.checkNumTopLabel.AutoSize = true;
+            this.checkNumTopLabel.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkNumTopLabel.Location = new System.Drawing.Point(480, 9);
+            this.checkNumTopLabel.Name = "checkNumTopLabel";
+            this.checkNumTopLabel.Size = new System.Drawing.Size(48, 18);
+            this.checkNumTopLabel.TabIndex = 0;
+            this.checkNumTopLabel.Text = "0000";
+            // 
+            // dateLabel
+            // 
+            this.dateLabel.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.dateLabel.AutoSize = true;
+            this.dateLabel.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateLabel.Location = new System.Drawing.Point(440, 33);
+            this.dateLabel.Name = "dateLabel";
+            this.dateLabel.Size = new System.Drawing.Size(88, 16);
+            this.dateLabel.TabIndex = 1;
+            this.dateLabel.Text = "__/__/____";
+            this.dateLabel.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(392, 70);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(16, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "$";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(394, 33);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Date";
+            // 
+            // dollarsLabel
+            // 
+            this.dollarsLabel.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.dollarsLabel.AutoSize = true;
+            this.dollarsLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.dollarsLabel.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dollarsLabel.Location = new System.Drawing.Point(414, 70);
+            this.dollarsLabel.Name = "dollarsLabel";
+            this.dollarsLabel.Size = new System.Drawing.Size(106, 18);
+            this.dollarsLabel.TabIndex = 4;
+            this.dollarsLabel.Text = "0,000,000.00";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(12, 9);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(44, 16);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Boelo";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(12, 70);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(77, 28);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "PAY TO THE\r\n ORDER OF";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // label9
+            // 
+            this.label9.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(443, 129);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(64, 16);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "Dollars";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(12, 191);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(40, 16);
+            this.label8.TabIndex = 9;
+            this.label8.Text = "MEMO";
+            // 
+            // payeeLabel
+            // 
+            this.payeeLabel.AutoSize = true;
+            this.payeeLabel.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.payeeLabel.Location = new System.Drawing.Point(90, 80);
+            this.payeeLabel.Name = "payeeLabel";
+            this.payeeLabel.Size = new System.Drawing.Size(278, 18);
+            this.payeeLabel.TabIndex = 10;
+            this.payeeLabel.Text = "___________________________";
+            // 
+            // dollarsTextLabel
+            // 
+            this.dollarsTextLabel.AutoSize = true;
+            this.dollarsTextLabel.Font = new System.Drawing.Font("Courier New", 10F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dollarsTextLabel.Location = new System.Drawing.Point(29, 129);
+            this.dollarsTextLabel.Name = "dollarsTextLabel";
+            this.dollarsTextLabel.Size = new System.Drawing.Size(408, 17);
+            this.dollarsTextLabel.TabIndex = 11;
+            this.dollarsTextLabel.Text = "__________________________________________________";
+            // 
+            // memoLabel
+            // 
+            this.memoLabel.AutoSize = true;
+            this.memoLabel.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.memoLabel.Location = new System.Drawing.Point(58, 189);
+            this.memoLabel.Name = "memoLabel";
+            this.memoLabel.Size = new System.Drawing.Size(218, 18);
+            this.memoLabel.TabIndex = 12;
+            this.memoLabel.Text = "_____________________";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label13.Location = new System.Drawing.Point(303, 193);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(217, 14);
+            this.label13.TabIndex = 13;
+            this.label13.Text = "______________________________";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Font = new System.Drawing.Font("Courier New", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label14.Location = new System.Drawing.Point(347, 170);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(140, 23);
+            this.label14.TabIndex = 14;
+            this.label14.Text = "// VOID //";
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Font = new System.Drawing.Font("Courier New", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label15.Location = new System.Drawing.Point(28, 219);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(109, 22);
+            this.label15.TabIndex = 15;
+            this.label15.Text = "123456789";
+            // 
+            // label16
+            // 
+            this.label16.AutoSize = true;
+            this.label16.Font = new System.Drawing.Font("Courier New", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label16.Location = new System.Drawing.Point(164, 219);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(142, 22);
+            this.label16.TabIndex = 16;
+            this.label16.Text = "325001090815";
+            // 
+            // checkNumBottomLabel
+            // 
+            this.checkNumBottomLabel.AutoSize = true;
+            this.checkNumBottomLabel.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkNumBottomLabel.Location = new System.Drawing.Point(386, 223);
+            this.checkNumBottomLabel.Name = "checkNumBottomLabel";
+            this.checkNumBottomLabel.Size = new System.Drawing.Size(48, 18);
+            this.checkNumBottomLabel.TabIndex = 19;
+            this.checkNumBottomLabel.Text = "0000";
+            // 
+            // label20
+            // 
+            this.label20.AutoSize = true;
+            this.label20.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label20.Location = new System.Drawing.Point(12, 32);
+            this.label20.Name = "label20";
+            this.label20.Size = new System.Drawing.Size(88, 16);
+            this.label20.TabIndex = 20;
+            this.label20.Text = "Intro Project 1";
+            // 
+            // totalsLabel
+            // 
+            this.totalsLabel.AutoSize = true;
+            this.totalsLabel.Location = new System.Drawing.Point(29, 250);
+            this.totalsLabel.Name = "totalsLabel";
+            this.totalsLabel.Size = new System.Drawing.Size(185, 13);
+            this.totalsLabel.TabIndex = 21;
+            this.totalsLabel.Text = "Total checks: 0 | Sum of all checks: 0";
+            // 
+            // OutputGUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(532, 272);
+            this.Controls.Add(this.totalsLabel);
+            this.Controls.Add(this.label20);
+            this.Controls.Add(this.checkNumBottomLabel);
+            this.Controls.Add(this.label16);
+            this.Controls.Add(this.label15);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.memoLabel);
+            this.Controls.Add(this.dollarsTextLabel);
+            this.Controls.Add(this.payeeLabel);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.dollarsLabel);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.dateLabel);
+            this.Controls.Add(this.checkNumTopLabel);
+            this.Name = "OutputGUI";
+            this.Text = "Boelo - Intro Project 1";
+            this.Load += new System.EventHandler(this.OutputGUI_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label checkNumTopLabel;
+        private System.Windows.Forms.Label dateLabel;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label dollarsLabel;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label payeeLabel;
+        private System.Windows.Forms.Label dollarsTextLabel;
+        private System.Windows.Forms.Label memoLabel;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label label16;
+        private System.Windows.Forms.Label checkNumBottomLabel;
+        private System.Windows.Forms.Label label20;
+        private System.Windows.Forms.Label totalsLabel;
     }
 }
 
