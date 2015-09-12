02283850659a2968c371f4b5de1117cd537c61b9
Cags23 <joesomebeast@gmail.com>
2015-09-12 17:04:16 -0500

    Tally

---
+++ IP02/IP01_Tally_Boelo.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IP1
+{
+    class IP01_Tally_Boelo
+    {
+        // values to be tracked by the tally component
+        private int numChecks = 3002; //set check counter to initial value of 3002
+
+        private int checksWrote = 0;
+
+        private double checkSum = 0;
+       
+        public object InForm { get; internal set; }
+
+        //increments total number of checks and check number when called
+        //input: none
+        //optput: none
+        public void incrementNumChecks()
+        {
+            numChecks++;
+
+            checksWrote++;
+        }
+
+        //adds up sum of checks
+        //input: user specified amount
+        //output: none
+        public void newSum(double amount)
+        {
+            checkSum = checkSum + amount;
+        }
+
+        //saftely returns the number of checks issued so far
+        //input: none
+        //output: numChecks (checkNumber) as int
+
+        public int getNumChecks()
+        {
+            return numChecks;
+        }
+
+        //saftely returns the number of checks issued so far
+        //input: none
+        //output: number of checks wrote as int
+
+        public int getNumChecksWrote()
+        {
+            return checksWrote;
+        }
+
+        //saftely returns the current check sum
+        //input: none
+        //output: checkSum (current sum of checks issued) as decimal
+
+        public double getCheckSum()
+        {
+            return checkSum;
+        }
+
+    }
+
+
+}
