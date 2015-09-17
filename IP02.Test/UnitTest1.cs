31d3affaf576220a278735cf54d70b7f522b69dd
Nicholas Kirk <nkirk@siue.edu>
2015-09-17 03:57:11 -0500

    Last commit of unit test cases

--- IP02.Test/UnitTest1.cs
+++ IP02.Test/UnitTest1.cs
@@ -1,11 +1,23 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace IP02.Test {
+namespace IP02 {
     [TestClass]
     public class UnitTest1 {
+
+        //Test the RunEngine class
         [TestMethod]
-        public void TestMethod1() {
+        public void TestRunEngine() {
+            RunEngine engine = new RunEngine();
+            engine.test("checkbook.cbk");
+        }
+
+        //Test the check display
+        [TestMethod]
+        public void TestOutputGUI() {
+            OutputGUI output = new OutputGUI();
+
+            output.displayCheck(1001, "test name", 125.50, "One hundred twenty five and 50/100", "test memo", 1, 125.5);
         }
     }
 }
