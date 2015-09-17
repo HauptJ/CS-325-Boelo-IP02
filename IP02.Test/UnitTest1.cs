e7eaff046dca3191bb5802a4278b0da9c6dc557e
Nicholas Kirk <nkirk@siue.edu>
2015-09-17 01:47:34 -0500

    Initial commit of unit test project

---
+++ IP02.Test/UnitTest1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace IP02.Test {
+    [TestClass]
+    public class UnitTest1 {
+        [TestMethod]
+        public void TestMethod1() {
+        }
+    }
+}
